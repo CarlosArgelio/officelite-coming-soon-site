e671d067871aaa97c5fe398c9d7d35f9dd49bcfc
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-13 00:55:46 -0400

    styles: config colors & font

--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -1,3 +1,7 @@
 export const Home = () => {
-  return <div className="text-xl text-blue-800 underline">Home</div>;
+  return (
+    <div className="text-xl text-blue-800 underline font-kumbh-sans">
+      A B C D E F G
+    </div>
+  );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,22 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brandeis-blue": "hsl(229, 100%, 66%)",
+        "cornflower-blue": "hsl(228, 100%, 75%)",
+        "prussian-blue": "hsl(229, 22%, 19%)",
+        "sunset-orange": "hsl(0, 83%, 65%)",
+        "slate-gray": "hsl(227, 13%, 52%)",
+        charcoal: "hsl(228, 22%, 26%)",
+      },
+      fontFamily: {
+        "kumbh-sans": ["Kumbh Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
